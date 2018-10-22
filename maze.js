99e398c73dd7c884801d85ac16f20c006f7aabd5
Jordan <actuaryspence@gmail.com>
2018-10-22 18:03:48 -0500

    Exercise 5

--- maze.js
+++ maze.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-	let walls = document.querySelectorAll(".boundary");
+    let walls = document.querySelectorAll(".boundary");
+    let win = false;
 	let hit_wall = false;
 	for(let index = 0; index < walls.length-1; index++){
 		walls[index].addEventListener("mouseover",function(){
@@ -7,29 +8,52 @@ window.onload = function(){
 			hit_wall = true;
 		});
 	}
-	let maze_game = document.getElementById("maze");
-	maze_game.addEventListener("mouseover",function(){
-		if(hit_wall === true){
-			for(let index = 0; index < walls.length-1; index++){
-				walls[index].setAttribute("class","boundary youlose");
-			}
-		}
-    });
     let start_game = document.getElementById("start");
 	start_game.addEventListener("mouseover",function(){
 		let end_game = document.getElementById("end");
 		end_game.addEventListener("mouseover",function(){
 			if(hit_wall === false){
-				alert("You Win!");
+                let game_status= document.getElementById("status");
+				if(game_status.innerHTML.search(" You Win!") === -1 && game_status.innerHTML.search(" You Lose!") === -1){
+					game_status.innerHTML = game_status.innerHTML + " You Win!";
+					win = true;
+                }
+            }else{
+				let game_status = document.getElementById("status");
+				if(game_status.innerHTML.search(" You Win!") === -1 && game_status.innerHTML.search(" You Lose!") === -1){
+					game_status.innerHTML = game_status.innerHTML + " You Lose!";
+				}
 			}
+
 		});
     });
     start_game.addEventListener("click",function(){
-		if(hit_wall === true){
-			for(let index = 0; index<walls.length-1; index++){
-				walls[index].setAttribute("class","boundary");
+		for(let index = 0; index<walls.length-1; index++){
+            walls[index].setAttribute("class","boundary");
+        }
+        if(win === false){
+			let game_status = document.getElementById("status");
+			let text_index = game_status.innerHTML.search(" You Lose!");
+			if(text_index !== -1){
+				game_status.innerHTML = game_status.innerHTML.slice(0,text_index);
 			}
-			hit_wall = false;
+		}
+		else{
+			let game_status = document.getElementById("status");
+			let text_index = game_status.innerHTML.search(" You Win!");
+			if(text_index !== -1){
+				game_status.innerHTML = game_status.innerHTML.slice(0,text_index);
+			}
+		}
+		hit_wall = false;
+		win = false;
+    });
+    let maze_game = document.getElementById("maze");
+	maze_game.addEventListener("mouseover",function(){
+		if(hit_wall === true){
+			for(let index = 0; index < walls.length-1; index++){
+				walls[index].setAttribute("class","boundary youlose");
+			}
 		}
 	});
 };
