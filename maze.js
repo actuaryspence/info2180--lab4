fc86859510f9b5ec64a5da5d71c15a96d8873895
actuaryspence <44044326+actuaryspence@users.noreply.github.com>
2018-10-22 17:24:36 -0500

    Delete maze.js

--- maze.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-    let wall = document.getElementById("boundary1");
-	wall.addEventListener("mouseover",function(){
-		wall.setAttribute("class","boundary youlose");
-	});
-};
